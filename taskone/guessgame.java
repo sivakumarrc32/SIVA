d2c710fd88f2ba8fe2eb0c94074856977d1b92ee
sivakumarrc32 <sivakumarrconline@gmail.com>
2024-08-03 14:50:19 +0530

    Update and rename taskone.java to guessgame.java

--- taskone/guessgame.java
+++ taskone/guessgame.java
@@ -3,7 +3,7 @@ package taskone;
 import java.util.Random;
 import java.util.Scanner;
 
-public class taskone{
+public class guessgame{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
